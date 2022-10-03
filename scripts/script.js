fbd322981813aa9ba0f2d8f0a37b455d82f91eab
Manamana <allentrobson@gmail.com>
2022-10-03 17:17:11 -0600

    HTML backbone and start of api call

---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+var body = $('#body');
+
+
+var apiKey = '4d63ba9d93efddcbcaf8047f7d2ec8b0';
+
+var city = 'salt lake city';
+
+async function searchCity(city) {
+    
+
+    requestUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${city.split(' ').join('+')},US&appid=${apiKey}`;
+
+        await fetch(requestUrl)
+        .then(function (response) {
+            if (response.status === 404) {
+                
+                console.log("City not found");
+              }
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        });
+}
+
+// searchCity(city);
+// searchCity("los angeles");
+// searchCity('sacramento');
+// searchCity('salem');
+
