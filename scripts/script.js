7a54b8bc2226858d4f2fb136cd8fabe1435c617c
Manamana <allentrobson@gmail.com>
2022-10-04 17:13:43 -0600

    Styling

--- scripts/script.js
+++ scripts/script.js
@@ -16,8 +16,8 @@ var previousSearchArray = [];
 
 var currentSearch;
 
-if (JSON.parse(localStorage.getItem("previousSearch")) !== null){
-previousSearchArray = JSON.parse(localStorage.getItem("previousSearch"));
+if (JSON.parse(localStorage.getItem("previousSearch")) !== null) {
+    previousSearchArray = JSON.parse(localStorage.getItem("previousSearch"));
 }
 
 // previousSearchArray = ['Los Angeles', 'San Diego', 'Salt Lake City'];
@@ -25,40 +25,55 @@ previousSearchArray = JSON.parse(localStorage.getItem("previousSearch"));
 
 
 async function searchCity(city) {
-    
+
 
     requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city.split(' ').join('+')},US&appid=${apiKey}&units=imperial`;
 
-        await fetch(requestUrl)
+    await fetch(requestUrl)
         .then(function (response) {
             if (response.status === 404) {
-                
+
                 console.log("City not found");
-              }
+            }
             return response.json();
         })
         .then(function (data) {
             console.log(data);
-            updateCard(data.name,data.main.temp,data.wind.speed,data.main.humidity,data.weather[0].description);
+            updateCard(data.name, data.main.temp, data.wind.speed, data.main.humidity, data.weather[0].description);
             searchCity5Day(data.name)
         });
 }
 async function searchCity5Day(city) {
-    
+
 
     requestUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${city.split(' ').join('+')},US&appid=${apiKey}&units=imperial`;
 
-        await fetch(requestUrl)
+    await fetch(requestUrl)
         .then(function (response) {
             if (response.status === 404) {
-                
+
                 console.log("City not found");
-              }
+            }
             return response.json();
         })
         .then(function (data) {
             console.log(data);
-            
+            console.log(data.list[4].dt_txt.split(' ')[0]);
+            for (let i = 0; i < 5; i++) {
+                if (i ===0) {
+                    update5Day(i, data.list[4].dt_txt.split(' ')[0], data.list[4].main.temp,data.list[4].wind.speed,data.list[4].main.humidity);
+                } else if (i ===1) {
+                    update5Day(i, data.list[12].dt_txt.split(' ')[0], data.list[12].main.temp,data.list[12].wind.speed,data.list[12].main.humidity);
+                } else if (i ===2) {
+                    update5Day(i, data.list[20].dt_txt.split(' ')[0], data.list[20].main.temp,data.list[20].wind.speed,data.list[20].main.humidity);
+                } else if (i ===3) {
+                    update5Day(i, data.list[28].dt_txt.split(' ')[0], data.list[28].main.temp,data.list[28].wind.speed,data.list[28].main.humidity);
+                } else if (i ===4) {
+                    update5Day(i, data.list[36].dt_txt.split(' ')[0], data.list[36].main.temp,data.list[36].wind.speed,data.list[36].main.humidity);
+                }
+                
+
+            }
 
         });
 }
@@ -76,37 +91,37 @@ async function searchCity5Day(city) {
 
 
 
-$('#searchText').on("keyup", function(e) {
+$('#searchText').on("keyup", function (e) {
     if (e.keyCode == 13) {
         searchRequest();
     }
 });
 
-function searchRequest(){
+function searchRequest() {
     currentSearch = $('#searchText').val();
     searchCity(currentSearch);
     updatePreviousSearchArray();
     populatePrevious();
-    
+
 
 }
 
-function updatePreviousSearchArray(){
+function updatePreviousSearchArray() {
     var saveSearch = true;
 
     previousSearchArray.forEach(search => {
 
-        if(search === currentSearch){
+        if (search === currentSearch) {
             saveSearch = false;
         }
-        
+
     });
 
-    if(saveSearch===true){
+    if (saveSearch === true) {
         previousSearchArray.unshift(currentSearch);
     }
 
-    if(previousSearchArray.length>10){
+    if (previousSearchArray.length > 10) {
         previousSearchArray.pop();
     }
 
@@ -114,39 +129,48 @@ function updatePreviousSearchArray(){
     localStorage.setItem("previousSearch", JSON.stringify(previousSearchArray));
 }
 
-function deleteButtons(){
+function deleteButtons() {
     buttonDivEl.empty();
 }
 
-function populatePrevious(){
+function populatePrevious() {
     deleteButtons();
 
     for (let i = 0; i < previousSearchArray.length; i++) {
-        
+
         var newButton = $('<button>');
-        newButton.attr('class', 'btn btn-secondary');
+        newButton.attr('class', 'btn btn-secondary m-1');
         newButton.attr('type', 'button');
         newButton.text(previousSearchArray[i]);
 
-        newButton.attr('onclick',`searchCity("${previousSearchArray[i]}")`);
+        newButton.attr('onclick', `searchCity("${previousSearchArray[i]}")`);
 
         buttonDivEl.append(newButton);
-        
+
     }
 }
 
-function updateCard(city, temp, wind, humidity, wDesc){
+function updateCard(city, temp, wind, humidity, wDesc) {
 
-    cardCityEl.text(`${city}`); 
+    cardCityEl.text(`${city}`);
     cardTempEl.text(`Temp: ${temp}°F`)
     cardWindEl.text(`Wind: ${wind} MPH`)
     cardHumidityEl.text(`Humidity: ${humidity}%`)
-    cardUvEl.text(`Weather Description: ${wDesc}`) 
+    cardUvEl.text(`Weather Description: ${wDesc}`)
 
 }
 
-function update5Day(){
-    
+function update5Day(index, date, temp, wind, humidity) {
+    var fiveDayDateEl = $(`#dayDate${index}`);
+    var fiveDayTempEl = $(`#dayTemp${index}`);
+    var fiveDayWindEl = $(`#dayWind${index}`);
+    var fiveDayHumidityEl = $(`#dayHumidity${index}`);
+
+    fiveDayDateEl.text(`${date}`);
+    fiveDayTempEl.text(`Temp: ${temp}°F`);
+    fiveDayWindEl.text(`Wind: ${wind}MPH`);
+    fiveDayHumidityEl.text(`Humidity: ${humidity}%`);
+
 }
 
 // updateCard("Atlanta", 74.01, 6.67, 46, .47);
