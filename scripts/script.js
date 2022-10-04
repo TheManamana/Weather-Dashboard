cbddc110bcabd959e31f8f8c41442399fc2d2678
Manamana <allentrobson@gmail.com>
2022-10-03 22:24:49 -0600

    Adding cardUpdate function

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,12 @@
 var body = $('#body');
 var buttonDivEl = $('#previousButtons');
 
+var cardCityEl = $('#cardCity');
+var cardTempEl = $('#temp');
+var cardWindEl = $('#wind');
+var cardHumidityEl = $('#humidity');
+var cardUvEl = $('#uv');
+
 
 var apiKey = '4d63ba9d93efddcbcaf8047f7d2ec8b0';
 
@@ -35,6 +41,8 @@ async function searchCity(city) {
             console.log(data);
         });
 }
+
+
 
 // searchCity(city);
 // searchCity("los angeles");
@@ -103,7 +111,17 @@ function populatePrevious(){
     }
 }
 
+function updateCard(city, temp, wind, humidity, uv){
 
+    cardCityEl.text(`${city}`); 
+    cardTempEl.text(`Temp: ${temp}°F`)
+    cardWindEl.text(`Wind: ${wind} MPH`)
+    cardHumidityEl.text(`Humidity: ${humidity}%`)
+    cardUvEl.text(`UV Index: ${uv}`) 
+
+}
+
+updateCard("Atlanta", 74.01, 6.67, 46, .47);
 populatePrevious();
 
 
